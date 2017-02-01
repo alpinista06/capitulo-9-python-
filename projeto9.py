71d60597c3223bc2cf4e9be6540e6e7d4b427db2
alpinista06 <mariod.b.filho@gmail.com>
2017-02-01 13:39:53 -0200

    projeto 9 para entregar

---
+++ projeto9.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python
+#--coding:utf8--
+
+
+import os, re, shutil
+
+def Preenchendo(prefixo, extensao, caminho=""):
+    if caminho == "":
+        caminho = os.getcwd()
+
+    os.chdir(caminho)
+
+    a = 1
+    l = 3
+    regex = re.compile(r'^%s(\d+)' % prefixo)
+    numList = []
+
+    lista = os.listdir(caminho)
+
+    for arquivo in lista:
+        if arquivo.endswith(extensao):
+            mo = regex.search(arquivo)
+            if mo != None:
+                c = mo.group(0)
+                numList.append(c)
+                renomeado = prefixo + str(a).zfill(l) + extensao
+                shutil.move(arquivo, renomeado)
+                numList.append(renomeado)
+                a += 1
+                print numList
+
+
+caminho = ""
+prefixo = "spam"
+extensao = ".txt"
+
+Preenchendo(prefixo, extensao, caminho)
